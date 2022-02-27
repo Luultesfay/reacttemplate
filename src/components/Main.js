e71a30b5a7a1740e6d779948b79374c641f20c24
luul negasi <luultesfay24@gmail.com>
2022-02-26 23:04:41 -0800

    intial commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>was first realeased in 2013 </li>
+        <li> was originally created by jordan walke</li>
+        <li> has well over 100K stars on GitHub</li>
+        <li> is maintained by FaceBook</li>
+        <li>Powers thousands of interprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+};
+export default Main;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import reactLogo from "../images/react-icon.png";
+
+const NavBar = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src={reactLogo} alt="reactlogo" width="67px" height="57px" />
+          <h3>ReactFacts</h3>
+          <h4>React Course- Project1</h4>
+        </nav>
+      </header>
+    </div>
+  );
+};
+export default NavBar;
